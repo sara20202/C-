641261e383de2703adc633eba70a88378d440c49
sara <zhurisara@outlook.com>
2021-05-24 11:00:02 +0200

    add ne project WEBAPIE for employee&department

---
+++ WEBAPIe/WebAPIE/Controllers/DepartmentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebAPIE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration; 
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                select DepartmentId , DepatmentName from dbo.Depatment";
+            DataTable table = new DataTable();
+
+        }
+    }
+}
